ff3d49d11ae520acf3f551974e43e6d164807189
Benjamin P Lee <benjaminplee@gmail.com>
2017-04-12 22:26:09 -0500

    WIP adding instrumentation tests - BL

--- app/src/androidTest/java/com/yardspoon/logmill/screen/viewer/ViewerActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/yardspoon/logmill/screen/viewer/ViewerActivityInstrumentedTest.java
@@ -1,21 +1,19 @@
 package com.yardspoon.logmill.screen.viewer;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
-
 @RunWith(AndroidJUnit4.class)
 public class ViewerActivityInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.yardspoon.logmill", appContext.getPackageName());
+    @Rule ActivityTestRule<ViewerActivity> rule = new ActivityTestRule<>(ViewerActivity.class);
+
+    @Test
+    public void fabClickLoadsLogsFromPresenter() throws Exception {
+
     }
 }
